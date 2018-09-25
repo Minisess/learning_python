fa0e808bce5040b3bcbdea898f7d30b69ca7b7dd
Minisess <38193469+Minisess@users.noreply.github.com>
2018-09-24 21:06:35 -0500

    Add files via upload

--- complete_factor.py
+++ complete_factor.py
@@ -9,17 +9,14 @@ factor_completely: which breaks an int into a list of prime factors
 from math import sqrt
 
 
-def factor_once(fac: int)-> list:
+def factor_once(fac: int):
     """
     factors any integer into a series of tuples containing two factors each
     is used by the factor completely function
-    """
-    results = [(1, fac)]
-    for fact1 in range(1, int(sqrt(fac)) + 1):
-        for fact2 in range(1, int((fac / 2) + 1)):
-            if fact1 * fact2 == fac:
-                results.append((fact1, fact2))
 
+    :return:  generator
+    """
+    results = ((x, fac // x) for x in range(int(sqrt(fac)+1), 0, -1) if fac % x == 0)
     return results
 
 
@@ -29,16 +26,16 @@ def factor_completely(int_to_factor: int)-> list:
     loops over every factor of a number and break it down into a list of only prime numbers needed to be multiplied together
 
     :param int_to_factor: needs to be an whole number
-    :return: returns a list of prime numbers that need to be multiplied to produce the original number
+    :return: a list of prime numbers that need to be multiplied to produce the original number
     """
-    step_one = factor_once(int_to_factor).pop()
+    step_one = next(factor_once(int_to_factor))
     if len(step_one) == 1:
         return [x for x in step_one]
     beginning = step_one
     while True:
         results = []
         for factor in beginning:
-            next_step = list(factor_once(factor).pop())
+            next_step = list(next(factor_once(factor)))
             if 1 in next_step:
                 next_step.remove(1)
             results.extend(list(next_step))
@@ -46,4 +43,3 @@ def factor_completely(int_to_factor: int)-> list:
             break
         beginning = results
     return results
-
